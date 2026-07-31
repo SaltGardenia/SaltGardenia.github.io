6b7760c78d0b1a0d389711e35f588ed763b5ce1c
SaltGardenia <saltgardenia@gmail.com>
2026-07-31 21:12:45 +0800

    update: change email to saltgardenia@gmail.com

--- vue-app/src/components/SocialLinks.vue
+++ vue-app/src/components/SocialLinks.vue
@@ -25,7 +25,7 @@ const links = [
   {
     id: 'email',
     name: t('social.email'),
-    url: 'mailto:icepaper135@gmail.com',
+    url: 'mailto:saltgardenia@gmail.com',
     icon: '<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 4.4-8 5-8-5V6l8 5 8-5z"/></svg>',
   },
   {
